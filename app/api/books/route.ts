bc63f90e32326c4b6f7041c6916784b89078f9bd
devazimi <azimihamidreza.work@gmail.com>
2026-04-27 17:52:04 +0330

    GET api books.json created

---
+++ app/api/books/route.ts
@@ -0,0 +1,16 @@
+import { promises as fs } from "fs";
+import { NextRequest, NextResponse } from "next/server";
+import path from "path";
+
+export default async function GET(req: NextRequest) {
+  try {
+    const filePath = path.join(process.cwd(), "books.json");
+    const fileContent = await fs.readFile(filePath + "./books.json", "utf-8");
+
+    const data = JSON.parse(fileContent);
+
+    return NextResponse.json({ success: true, data });
+  } catch (err) {
+    return NextResponse.json({ error: "failed fetch data" }, { status: 500 });
+  }
+}
