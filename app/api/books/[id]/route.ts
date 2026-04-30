68dc4e57725c86b71f90ca1a34fc8d42b4fe0386
devazimi <azimihamidreza.work@gmail.com>
2026-04-30 22:06:33 +0330

    book[id] get api created

---
+++ app/api/books/[id]/route.ts
@@ -0,0 +1,27 @@
+import { NextResponse, NextRequest } from "next/server";
+import { promises as fs } from "fs";
+import path from "path";
+
+export interface Params {
+  params: Promise<{ id: string }>;
+}
+
+export async function GET(req: NextRequest, { params }: Params) {
+  try {
+    const { id } = await params;
+    const filePath = path.join(process.cwd(), "bookPersian.json");
+    const fileContent = await fs.readFile(filePath, "utf-8");
+    const data = JSON.parse(fileContent);
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const book = data.find((d: any) => d.id === id);
+
+    if (!book) {
+      return NextResponse.json({ error: "book not found" }, { status: 400 });
+    }
+
+    return NextResponse.json(book);
+  } catch (err) {
+    return NextResponse.json({ error: "failed request" }, { status: 500 });
+  }
+}
