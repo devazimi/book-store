d07933bac7a0d83624d1f4e65573fa8c63b61629
devazimi <azimihamidreza.work@gmail.com>
2026-05-03 19:47:17 +0330

    signup page changed, it has a better error handling for ux/ui

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,16 +1,17 @@
 "use client";
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 import { z } from "zod";
 
 const zodFormSchema = z
   .object({
-    username: z.string().min(5, "username must be 5 characters at least"),
-    email: z.string().email("invalid email format"),
-    password: z.string().min(8, "password must be 8 characters at least"),
+    username: z.string().min(5, "نام کاربری باید حداقل 5 حرف باشد"),
+    email: z.string().email("فرمت ایمیل نا معتبر است"),
+    password: z.string().min(8, "پسورد باید حداقل 8 حرف باشد"),
     confirmPassword: z.string(),
   })
   .refine((data) => data.confirmPassword === data.password, {
-    message: "password is not match",
+    message: "پسورد صحیح نمی باشد",
     path: ["confirmPassword"],
   });
 
@@ -19,22 +20,31 @@ export default function SignUpPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+  const [errors, setErrors] = useState<Record<string, string>>({});
+
+  const router = useRouter();
 
   const registerUser = async (e: React.FormEvent) => {
     e.preventDefault();
+
+    setErrors({});
+
+    const body = { username, email, password, confirmPassword };
+
+    const parsed = zodFormSchema.safeParse(body);
+
+    if (!parsed.success) {
+      console.log("parsed error: ", parsed.error.flatten());
+      const feildErrors: Record<string, string> = {};
+      parsed.error.issues.forEach((issue) => {
+        const field = issue.path[0] as string;
+        feildErrors[field] = issue.message;
+      });
+      setErrors(feildErrors);
+      return;
+    }
+
     try {
-      const body = { username, email, password, confirmPassword };
-
-      const parsed = zodFormSchema.safeParse(body);
-
-      if (!parsed.success) {
-        console.log("parsed error: ", parsed.error.flatten());
-        return parsed.error.issues.map((issue: any) => ({
-          field: issue.path.join("."),
-          message: issue.message,
-        }));
-      }
-
       const res = await fetch(`/api/signup`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
@@ -49,6 +59,7 @@ export default function SignUpPage() {
       const data = await res.json();
       console.log("data: ", data);
 
+      router.push("/login?registered=true");
       return data;
     } catch (err) {
       console.log("error: ", err);
@@ -69,48 +80,107 @@ export default function SignUpPage() {
         </h1>
         {/* input */}
         <input
-          className="w-full border border-gray-300 rounded-xl px-4 py-3 text-right transition-colors
-              focus:outline-none focus:border-[#4b7995] focus:ring-2 focus:ring-[#4b7995]/20"
+          className={`w-full border rounded-xl px-4 py-3 text-right transition-colors
+              focus:outline-none focus:border-[#4b7995] focus:ring-2 focus:ring-[#4b7995]/20 
+              ${errors.email ? `border-red-200` : `border-gray-300`}
+              `}
           aria-label="username..."
           placeholder="نام کاربری..."
           value={username}
-          onChange={(e) => setUsername(e.target.value)}
+          onChange={(e) => {
+            setUsername(e.target.value);
+            if (errors.username) {
+              setErrors((prev) => {
+                const newErrors = { ...prev };
+                delete newErrors.username;
+                return newErrors;
+              });
+            }
+          }}
+          required
         />
+        {errors.username && (
+          <p className="text-red-500 text-xs text-right">{errors.username}</p>
+        )}
         {/* input */}
         <input
-          className="w-full border border-gray-300 rounded-xl px-4 py-3 text-right transition-colors
-              focus:outline-none focus:border-[#4b7995] focus:ring-2 focus:ring-[#4b7995]/20"
-          aria-label="username..."
+          className={`w-full border rounded-xl px-4 py-3 text-right transition-colors
+              focus:outline-none focus:border-[#4b7995] focus:ring-2 focus:ring-[#4b7995]/20 
+              ${errors.email ? `border-red-200` : `border-gray-300`}
+              `}
+          aria-label="email..."
           placeholder="آدرس ایمیل..."
           value={email}
-          onChange={(e) => setEmail(e.target.value)}
+          onChange={(e) => {
+            setEmail(e.target.value);
+            if (errors.email) {
+              setErrors((prev) => {
+                const newErrors = { ...prev };
+                delete newErrors.email;
+                return newErrors;
+              });
+            }
+          }}
+          required
         />
-
+        {errors.email && (
+          <p className="text-red-500 text-xs text-right">{errors.email}</p>
+        )}
         {/* input */}
         <input
-          className="w-full border border-gray-300 rounded-xl px-4 py-3 text-right transition-colors
-              focus:outline-none focus:border-[#4b7995] focus:ring-2 focus:ring-[#4b7995]/20"
-          aria-label="username..."
+          className={`w-full border rounded-xl px-4 py-3 text-right transition-colors
+              focus:outline-none focus:border-[#4b7995] focus:ring-2 focus:ring-[#4b7995]/20 
+              ${errors.password ? `border-red-200` : `border-gray-300`}
+              `}
+          aria-label="password"
           placeholder="کلمه عبور..."
           value={password}
-          onChange={(e) => setPassword(e.target.value)}
+          onChange={(e) => {
+            setPassword(e.target.value);
+            if (errors.password) {
+              setErrors((prev) => {
+                const newErrors = { ...prev };
+                delete newErrors.password;
+                return newErrors;
+              });
+            }
+          }}
+          required
         />
-
+        {errors.password && (
+          <p className="text-red-500 text-xs text-right">{errors.password}</p>
+        )}
         {/* input */}
         <input
-          className="w-full border border-gray-300 rounded-xl px-4 py-3 text-right transition-colors
-              focus:outline-none focus:border-[#4b7995] focus:ring-2 focus:ring-[#4b7995]/20"
-          aria-label="username..."
+          className={`w-full border rounded-xl px-4 py-3 text-right transition-colors
+              focus:outline-none focus:border-[#4b7995] focus:ring-2 focus:ring-[#4b7995]/20 
+              ${errors.confirmPassword ? `border-red-200` : `border-gray-300`}
+              `}
+          aria-label="confirmPassword"
           placeholder="تکرار کلمه عبور..."
           value={confirmPassword}
-          onChange={(e) => setConfirmPassword(e.target.value)}
+          onChange={(e) => {
+            setConfirmPassword(e.target.value);
+            if (errors.confirmPassword) {
+              setErrors((prev) => {
+                const newErrors = { ...prev };
+                delete newErrors.confirmPassword;
+                return newErrors;
+              });
+            }
+          }}
+          required
         />
-
+        {errors.confirmPassword && (
+          <p className="text-red-500 text-xs text-right">
+            {errors.confirmPassword}
+          </p>
+        )}
         {/* button */}
         <button
           type="submit"
-          className="mt-2 w-full py-3 bg-[#4b7995] text-white rounded-xl transition-colors hover:bg-[#3a6178]
-           focus:ring-2 focus:ring-gray-[#4b7995]/50 focus:outline-none"
+          className="mt-2 w-full py-3 bg-[#4b7995] text-white rounded-xl transition-colors 
+          hover:bg-[#3a6178] focus:ring-2 focus:ring-gray-[#4b7995]/50 focus:outline-none cursor-pointer"
         >
           ثبت نام
         </button>
